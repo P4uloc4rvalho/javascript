2e9bf2e464af115613da8bdf072bd6ed6fcd0359
Paulo Carvalho <119765506+P4uloc4rvalho@users.noreply.github.com>
2024-03-16 19:19:36 -0300

    Repetição com for
    estrutura de repetição for e var compostas.
    com comentários...

--- cursojs/aula15/vetornatela.js
+++ cursojs/aula15/vetornatela.js
@@ -1,12 +1,10 @@
+let valor = [0, 1, 7, 4, 2, 9] /**vars compostas .*/
+valor.sort() /**colocando a var valor em ordem crescente. */
+console.log(valor) /**mostrando a var valor */
+for (let pos in valor) { /**for é uma estrutura de repetição, e a var pos está guradando os dados da var valor. */
+    console.log(`A posição ${pos} tem valor ${valor[pos]}`)
+}/**a var pos mostra a posição das vars, a var valor[pos] mostra o dado salvo na var. */
 
-let valor = [0, 1, 7, 4, 2, 9]
-valor.sort()
-console.log(valor)
-/*
-for(let pos=0 ; pos<valor.length ; pos++){
-    console.log(`A posção ${pos} tem o valor ${valor[pos]}`)
-}*/
-
-for (let pos in valor) {
+/** for (let pos=0 ; pos<valor.length ; pos++){
     console.log(`A posição ${pos} tem o valor ${valor[pos]}`)
-}
+  }  outro jeito de fazer a mesma repetição...*/
